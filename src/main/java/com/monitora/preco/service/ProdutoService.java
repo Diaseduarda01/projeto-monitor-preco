367c9bf9b8b9c8c06e5e9294a09c74d2c568c423
Eduarda Dias <maria.dias@sptech.school>
2025-07-25 23:16:04 -0300

    feat(app): Criando service para produto

---
+++ src/main/java/com/monitora/preco/service/ProdutoService.java
@@ -0,0 +1,46 @@
+package com.monitora.preco.service;
+
+import com.monitora.preco.entity.Produto;
+import com.monitora.preco.exception.ProdutoNaoEncontrado;
+import com.monitora.preco.repository.ProdutoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ProdutoService {
+
+    private final ProdutoRepository repository;
+
+    public Produto salvar(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public void deletar (BigInteger id) {
+        Produto produto = buscarPorId(id);
+        repository.deleteById(id);
+    }
+
+    public Produto buscarPorId(BigInteger id) {
+        return repository.findById(id).orElseThrow(ProdutoNaoEncontrado::new);
+    }
+
+    public List<Produto> buscarTodos() {
+        return repository.findAll();
+    }
+
+    public Produto atualizar(BigInteger id, Produto produto) {
+        Produto produtoExistente = buscarPorId(id);
+
+        produtoExistente.setNome(produto.getNome());
+        produtoExistente.setUrl(produto.getUrl());
+        produtoExistente.setAtivo(produto.getAtivo());
+        produtoExistente.setPrecoDesejado(produto.getPrecoDesejado());
+
+        return salvar(produtoExistente);
+    }
+
+}

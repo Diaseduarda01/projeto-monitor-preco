b050b09d6a8a2221bc2e069fb0e0335bc731449a
Eduarda Dias <maria.dias@sptech.school>
2025-07-28 11:37:46 -0300

    feat(app): Criando service para role

---
+++ src/main/java/com/monitora/preco/service/RoleService.java
@@ -0,0 +1,19 @@
+package com.monitora.preco.service;
+
+import com.monitora.preco.entity.Role;
+import com.monitora.preco.exception.naoencontrado.RoleNaoEncontradoException;
+import com.monitora.preco.repository.RoleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RoleService {
+
+    private final RoleRepository repository;
+
+    public Role buscarPorNome(String nome) {
+        return repository.findByNome(nome)
+                .orElseThrow(RoleNaoEncontradoException::new);
+    }
+}

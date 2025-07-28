7119fd67847e3da0e132271be8b0037d525e97b9
Eduarda Dias <maria.dias@sptech.school>
2025-07-28 11:37:31 -0300

    feat(app): Criando repository para role

---
+++ src/main/java/com/monitora/preco/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.monitora.preco.repository;
+
+import com.monitora.preco.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByNome(String nome);
+}

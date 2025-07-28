ff9f8fd0dfea340a5cc63fb9a363408595930aee
Eduarda Dias <maria.dias@sptech.school>
2025-07-28 16:15:32 -0300

    refactor(app): Alteração de todo

--- src/main/java/com/monitora/preco/PrecoApplication.java
+++ src/main/java/com/monitora/preco/PrecoApplication.java
@@ -15,31 +15,6 @@ public class PrecoApplication {
 	//Agendamento periódico:
 	//@Scheduled para executar uma tarefa periodicamente (ex: a cada 1 hora, 1 dia, 1 Semana)
 
-	// TODO: DASHBOARD INDIVIDUAL POR PRODUTO
-	// KPIs:
-		// GET /dashboard/produto/{produtoId}/preco-desejado
-		// → Retornar preço desejado do produto
-
-		// GET /dashboard/produto/{produtoId}/preco-atual
-		// → Retornar o último preço coletado
-
-		// GET /dashboard/produto/{produtoId}/variacao
-		// → Retornar variação percentual entre primeiro e último preço monitorado
-
-		// GET /dashboard/produto/{produtoId}/tempo
-		// → Dias entre primeira e última coleta (tempo monitorado)
-
-	// TODO: RELATÓRIOS
-		// POST /usuarios/{usuarioId}/relatorios?tipo=semanal ou mensal
-		// → Gerar relatório PDF ou similar (histórico e resumo)
-
-		// GET /usuarios/{usuarioId}/relatorios
-		// → Listar relatórios já gerados pelo usuário
-
-		// GET /relatorios/{relatorioId}/download
-		// → Baixar o relatório específico (PDF, CSV, etc)
-
-
 	// TODO: MELHORIAS FUTURAS (IDEIAS)
 		// Filtro de datas na dashboard individual
 		// Suporte para múltiplos usuários (admin vê todos, comum vê os próprios)
